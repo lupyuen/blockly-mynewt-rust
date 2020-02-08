53e7957e6ee7504337c53d81ce44773e6521fc39
lupyuen <luppy@appkaki.com>
2020-02-08 20:35:20 +0800

    Generated label and button code

--- generators/rust/app_code.js
+++ generators/rust/app_code.js
@@ -12,12 +12,18 @@ Blockly.Rust['app'] = function(block) {
             Blockly.Rust.ORDER_NONE) || '\'\'';
   }
   var code = [
-    'app!( @json {',
-    //  Insert the indented elements.
+    '//  Create a column',
+    'let mut col = Column::new();',
+    '//  Add the widgets to the column',
+    //  Insert the elements.
+    elements.join('\n'),
+    /*
     Blockly.Rust.prefixLines(
-      elements.join(',\n'), 
+      elements.join('\n'), 
       Blockly.Rust.INDENT),
-    '})',
+    */
+    '//  Return the column containing the widgets',
+    'col',
   ].join('\n');
   return [code, Blockly.Rust.ORDER_UNARY_POSTFIX];
 };
@@ -27,10 +33,17 @@ Blockly.Rust['label'] = function(block) {
   var text_name = block.getFieldValue('NAME');
   var value_name = Blockly.Rust.valueToCode(block, 'name', Blockly.JavaScript.ORDER_ATOMIC);
   var code = [
-    '"', text_name, '"',
-    ': ',
-    value_name,
-  ].join('');
+    '//  Add the label widget to the column, centered with padding',
+    'col.add_child(',
+      'Align::centered(',
+        'Padding::new(5.0, ',
+          text_name,
+        ')',
+      '),',
+      '1.0',
+    ');',
+  ].join('\n');
+
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Rust.ORDER_NONE];
 };
@@ -40,10 +53,14 @@ Blockly.Rust['button'] = function(block) {
   var text_name = block.getFieldValue('NAME');
   var value_name = Blockly.Rust.valueToCode(block, 'name', Blockly.JavaScript.ORDER_ATOMIC);
   var code = [
-    '"', text_name, '"',
-    ': ',
-    value_name,
-  ].join('');
+    '//  Add the button widget to the column, with padding',
+    'col.add_child(',
+      'Padding::new(5.0, ',
+        text_name,
+      ')',
+    '1.0',
+    ');',
+  ].join('\n');
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Rust.ORDER_NONE];
 };
